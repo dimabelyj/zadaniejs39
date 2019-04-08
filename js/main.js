32cc473af6bf8f46eeddfb777a35d9185ad913a9
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-04-08 22:52:45 +0200

    nie dziala

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 let fr = [1, 2, 3, 4, 5];
-fr[0] = 6;
-fr[1] = 7;
-console.log(fr);
+//let pr = fr.splice(myFunction);
+
+function myFunction() {
+    //fr[0] = 6;
+    //fr[1] = 7;
+    //return value * 2;
+    fr.splice(2, 2, 6, 7);
+    //return fr;
+    console.log(fr);
+}
+//console.log(myFunction());
