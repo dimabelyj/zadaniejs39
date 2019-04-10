0968ae7f337f693b9e18774f598b4e212cedcafc
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-04-10 19:29:14 +0200

    blagam o pomoc

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-let fr = [1, 2, 3, 4, 5];
+let xz = [1, 2, 3, 4, 5];
 //let pr = fr.splice(myFunction);
 
 function myFunction() {
+    let x = xz;
     //fr[0] = 6;
     //fr[1] = 7;
     //return value * 2;
-    fr.splice(2, 2, 6, 7);
+    x.splice(2, 2, 6, 7);
     //return fr;
-    console.log(fr);
+    //console.log(ja);
 }
-//console.log(myFunction());
+console.log(myFunction(xz));
